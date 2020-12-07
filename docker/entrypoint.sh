39c741c035c8d1bd43c5e44b37037433a2acdb94
Skynet Core <skynetvasyl@gmail.com>
2020-12-07 15:41:16 +0200

    release

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "NFPM package version: $NFPM_VERSION"
-
 if [ "x$INPUT_CONFIG" != "x" ] && [ "x$INPUT_PACKAGER" != "x" ]; then
     if [ "x$INPUT_TARGET" != "x" ]; then
         $name=$(sudo sh -c "nfpm pkg -f $INPUT_CONFIG -p $INPUT_PACKAGER -t $INPUT_TARGET" | grep "created package:" | sed 's|created package: ||g')
